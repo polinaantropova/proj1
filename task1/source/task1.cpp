715ebbcb5a2a16223f1ff8473fd4ad8bd0037d91
polinaantropova <st095276@spbu.student.ru>
2021-10-08 23:41:26 +0300

    добавлены функции для 3 и 4 задач

--- task1/source/task1.cpp
+++ task1/source/task1.cpp
@@ -16,15 +16,55 @@ void task2(int n) {
 	}
 
 }
+void task3(int n) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = n-i; j > 0; --j) {
+			std::cout << " ";
+		} 
+		for (int k = 0; k < i+1; ++k) {
+			std::cout << "*";
+
+		}
+		std::cout << std::endl;
+	} 
+}
+void task4 (int n) {
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 0; j < n-i; ++j) {
+			std::cout << " ";
+		}
+		for (int k = 0; k < 2*i-1; ++k) {
+			std::cout << "*";
+
+		}
+		std::cout << std::endl;
+	}
+	for (int i = n-1; i >=1; --i) {
+		for (int j = 0; j < n - i; ++j) {
+			std::cout << " ";
+		}
+		for (int k = 0; k < 2 * i - 1; ++k) {
+			std::cout << "*";
+
+		}
+		std::cout << std::endl;
+	}
+}
 
 int main()
 
 {
-	int n=-1;
+	int n = -1;
 	std::cout << "Input n ";
 	do {
-		std::cin >> n ;
-	} while (n < 0) ;
+		std::cin >> n;
+	} while (n < 0);
+	task1(n);
+	std::cout << std::endl;
 	task2(n);
-} 
+	std::cout << std::endl;
+	task3(n);
+	std::cout << std::endl;
+	task4(n);
+}
 
