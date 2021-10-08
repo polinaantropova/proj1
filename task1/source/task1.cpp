4096bcba5570b482ce327382cdd28ec88f07387d
polinaantropova <st095276@spbu.student.ru>
2021-10-08 21:58:10 +0300

    task1

---
+++ task1/source/task1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+void task1(int n) {
+	for (int i = 0; i <= n; ++i) {
+		for (int j = 0; j < i; ++j) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+}
+void task2(int n) {
+	for (int i = 0; i <= n; ++i) {
+		for (int j = n; j > i; --j) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+
+}
+
+int main()
+
+{
+	int n=-1;
+	std::cout << "Input n ";
+	do {
+		std::cin >> n ;
+	} while (n < 0) ;
+	task2(n);
+} 
+
