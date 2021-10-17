7e7799391c7014d386b37d43a0fe66689271af2d
polinaantropova <st095276@spbu.student.ru>
2021-10-17 22:41:44 +0300

    решение задачи 3

---
+++ 11_10_2021/task3/build/gusi_kroliki.cpp
@@ -0,0 +1,23 @@
+﻿// gusi_kroliki.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int i;
+    std::cout << " Input half count paws = ";
+    std::cin >> i;
+    i = i * 2;
+    std::cout << "total paws = " << i << "\n";
+    int g = 0;
+    g = (i % 4) / 2; // остаток лап, при макс. кол-ве кроликов
+    
+    for (int j = i / 4; j >= 0; --j) {
+        std::cout << j << " rabbits, " << g << " gusi" << "\n";
+        g += 2; // -1 кролик, +2 гуся
+    }
+}
+
+
+
