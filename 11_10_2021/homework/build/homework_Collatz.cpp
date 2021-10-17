4774754416e9ea103a854ce92773c68f420c8035
polinaantropova <st095276@spbu.student.ru>
2021-10-18 02:46:44 +0300

    решение домашней задачи

---
+++ 11_10_2021/homework/build/homework_Collatz.cpp
@@ -0,0 +1,28 @@
+﻿// homework_Collatz.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+        int n, k=0;
+        std::cout << "Input n= ";
+        std::cin >> n;
+        std::cout << n ;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+                ++k;
+                std::cout << ",   " << n;
+            }
+            else {
+                n = 3 * n + 1;
+                ++k;
+                std::cout << ",   " << n;
+            }
+        }
+        std::cout << " ...\n";
+        std::cout << "total " << k << " steps";
+}
+
