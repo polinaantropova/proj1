04f22d6cf6b1bcef7e45bc06a97c109cec066998
polinaantropova <st095276@spbu.student.ru>
2021-10-18 02:04:25 +0300

    решение задачи 4

---
+++ 11_10_2021/task4/build/pi.cpp
@@ -0,0 +1,21 @@
+﻿// pi.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+    int n;
+    double pi = 0.0;
+    double PI = 0.0;
+    std::cout << " Input accuracy = ";
+    std::cin >> n; 
+    for (int i = 0; i < n; ++i) {
+        pi = 1.0 / (2 * i + 1) - 1.0 / (2 * i + 3);
+        PI += pi;
+    }
+    std::cout << "pi= "<<4*PI;
+  
+}
+
