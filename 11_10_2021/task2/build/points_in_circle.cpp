bd456c1575c4c5dac7715af669c9a4a06266e029
polinaantropova <st095276@spbu.student.ru>
2021-10-18 02:11:18 +0300

    решение задачи 2

---
+++ 11_10_2021/task2/build/points_in_circle.cpp
@@ -0,0 +1,23 @@
+﻿// points_in_circle.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    //int c_x = 0, c_y = 0;
+    double R;
+    std::cout << " Input radius R = ";
+    std::cin >> R;
+    std::cout << "\n";
+    int k = 0;
+    for (int x = -R; x <= R; ++x) {
+        for (int y = -R; y <= R; ++y) {
+            if (x * x + y * y <= R * R) { ++k; };
+        }
+    }
+    
+    std::cout << "Points =" << k;
+
+}
+
