674487df58f0d5a04132993fc92344cd591d561a
polinaantropova <st095276@spbu.student.ru>
2021-10-18 01:03:10 +0300

    добавлено решение задачи 8

---
+++ 11_10_2021/task8/source/zifryvchisle.cpp
@@ -0,0 +1,21 @@
+﻿// zifryvchisle.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{//решение для int
+    int a;
+    std::cout << " Input a : ";
+    std::cin >> a;
+    std::cout << "\n";
+	
+	int sum = 0;
+	int product = 1;
+	while (a > 0) {
+		sumOfDigits += a % 10;
+		product *= a % 10;
+		a /= 10;
+	}
+	std::cout << "sum = " << sum << ", product = " << product << "\n";
+}
