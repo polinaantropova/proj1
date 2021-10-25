eff1f83510e62ba8bd761a1dba9fa389d8956c6d
polinaantropova <st095276@spbu.student.ru>
2021-10-25 12:52:02 +0300

    генерирование 16ричного числа

---
+++ 25_10_2021/task 2 (16rich array)/source/task2.cpp
@@ -0,0 +1,35 @@
+﻿// task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+
+
+
+int main()
+{
+    
+    int i;
+
+    srand(time(0));
+    int size = 0;
+    std::cout << "input number symbols in number : ";
+    std::cin >> size;
+    
+
+
+    //массив с элементами-цифрами 16ричной сс
+    char c[16] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
+    // for (i = 0; i < 16; ++i)
+         //std::cout << c[i] << " ";
+
+    //выводим 16ричное число заданного размера
+    char* l = new char[size];
+    for (int i = 0; i < size; ++i) {
+        l[i] = c[rand() % 16];
+        std::cout << l[i];
+    }
+    std::cout << "\n";
+    delete[] l;
+}
+
