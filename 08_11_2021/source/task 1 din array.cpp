5e4f13cdf567efc8ccc6815123aeb7fb29a9cdb1
polinaantropova <st095276@spbu.student.ru>
2021-11-14 19:42:21 +0300

    добавлена диагональная змейка

--- 08_11_2021/source/task 1 din array.cpp
+++ 08_11_2021/source/task 1 din array.cpp
@@ -27,29 +27,22 @@ void initialize(int** A, int row, int column, int start1) {
 void createSpiral (int** A, int row, int column, int start1) {
     
         for (int i = 0; i < column/2 +1; ++i) {
-
-        for (int j = i; j < column-i; ++j) {
-            
+        for (int j = i; j < column-i; ++j) {  
             A[i][j] = start1++;
         }
-       
         for (int k = i+1; k < column - i; ++k) {
-
             A[k][column -1 -i] = start1++;
         }
-
         for (int j = column -i-2; j >=i; --j) {
-
             A[column-i-1][j] = start1++;
         }
-
         for (int k = column -2 -i; k > i; --k) {
-
             A[k][i] = start1++;
         }    
     }
         
 }
+
 // змейка
 void createSnake(int** A, int row, int column, int start1) {
 
@@ -65,13 +58,50 @@ void createSnake(int** A, int row, int column, int start1) {
                 A[i][j] = start1++;
             }
         }
-  
     }
-
 }
 
+// диагональная змейка
+void createDiagSnake_right(int** A, int row, int column, int start1) {
+    int count_diag = row + column - 1;
+    for (int p = 1; p <= count_diag; ++p) {  
+        for (int i = 0, j = p - 1; i < p; ++i, --j) {
+            if (p % 2 == 0) {
+                if (i < row && j < column) {
+                    A[i][j] = start1++;
+                } else {
+                    continue;
+                }
+            } else {
+                if (j < row && i < column) {
+                    A[j][i] = start1++;
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+}
 
-void printArray(int** A, int row, int column) {
+void createDiagSnake_down(int** A, int row, int column, int start1) {
+    int count_diag = row + column - 1;
+    for (int p = 1; p <= count_diag; ++p) {
+        for (int i = 0, j = p - 1; i < p; ++i, --j) {
+            if (p % 2 != 0) {
+                if (i < row && j < column) {
+                    A[i][j] = start1++;
+                } else continue;
+                
+            } else {
+                if (j < row && i < column) {
+                    A[j][i] = start1++;
+                } else continue;
+            }
+        }
+    }
+}
+
+void printArray (int** A, int row, int column) {
     for (int i = 0; i < row; ++i) {
         for (int j = 0; j < column; ++j) {
             
@@ -107,21 +137,49 @@ int main()
     std::cin >> row >> column;
     std::cout << "start number : \n";
     std::cin >> start1;
-    
+
 
     int** A;
-    A = create(row, column); 
-    std::cout << "arrage in order" << std::endl;
-    initialize(A, row, column, start1); 
+    A = create(row, column);
+    std::cout << "\nchoose the task:\n";
+    std::cout << "1. arrage in order\n" << "2.arrage-spiral (only square)\n";
+    std::cout << "3.arrage - snake\n" << "4.arrage - Diag_snake_right\n" << "5.arrage - Diag_snake_down\n";
+    int choice;
+    std::cout <<"\nyour choice :" << std::endl;
+    std::cin >> choice;
     std::cout << std::endl;
-    std::cout << "arrage-spiral" << std::endl;
+    switch (choice) {
+    case 1: std::cout << "arrage in order" << std::endl;
+    initialize(A, row, column, start1);
+    std::cout << std::endl;
+    break;
+
+    case 2:std::cout << "arrage-spiral" << std::endl;
     createSpiral(A, row, column, start1);
     printArray(A, row, column);
     std::cout << std::endl;
-    std::cout << "arrage-snake" << std::endl;
+    break;
+
+    case 3:std::cout << "arrage-snake" << std::endl;
     createSnake(A, row, column, start1);
     printArray(A, row, column);
     std::cout << std::endl;
+    break;
+
+    case 4:std::cout << "arrage-Diag_snake_right" << std::endl;
+    createDiagSnake_right(A, row, column, start1);
+    printArray(A, row, column);
+    std::cout << std::endl;
+    break;
+
+    case 5:std::cout << "arrage-Diag_snake_down" << std::endl;
+    createDiagSnake_down(A, row, column, start1);
+    printArray(A, row, column);
+    std::cout << std::endl;
+
+    default:std::cout << "\nsorry, you put a wrong number...\n";
+}
+
     for (int i = 0; i < row; i++)
     {
         delete[] A[i];
