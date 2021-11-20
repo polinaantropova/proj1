f7a178a82bc06993777f9ca9adbe84d08300915b
polinaantropova <st095276@spbu.student.ru>
2021-11-20 16:51:50 +0300

    добавлена универсальная спираль

--- 08_11_2021/build/task 1 din array.cpp
+++ 08_11_2021/build/task 1 din array.cpp
@@ -5,37 +5,155 @@
 
 int** create(int row, int column) {
     int** A;
-    A = new int *[row];
+    A = new int* [row];
     for (int i = 0; i < row; ++i) {
         A[i] = new int[column];
     }
     return A;
 }
 
-void initialize (int **A, int row, int column, int start1) {
+void initialize(int** A, int row, int column, int start1) {
     for (int i = 0; i < row; ++i) {
         for (int j = 0; j < column; ++j) {
-            A[i][j] = start1 + i + j;
+
+            A[i][j] = start1;
+            ++start1;
             std::cout << A[i][j] << "\t";
         }
         std::cout << std::endl;
+    }
+}
+// квадратная спираль
+void createSpiral(int** A, int row, int column, int start1) {
+
+    for (int i = 0; i < column / 2 + 1; ++i) {
+        for (int j = i; j < column - i; ++j) {
+            A[i][j] = start1++;
+        }
+        for (int k = i + 1; k < column - i; ++k) {
+            A[k][column - 1 - i] = start1++;
+        }
+        for (int j = column - i - 2; j >= i; --j) {
+            A[column - i - 1][j] = start1++;
+        }
+        for (int k = column - 2 - i; k > i; --k) {
+            A[k][i] = start1++;
+        }
+    }
+
+}
+
+// змейка
+void createSnake(int** A, int row, int column, int start1) {
+
+    for (int i = 0; i < row; ++i) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < column; ++j) {
+                A[i][j] = start1++;
+            }
+        }
+        if (i % 2 != 0) {
+            for (int j = column - 1; j >= 0; --j) {
+
+                A[i][j] = start1++;
+            }
+        }
     }
 }
 
-void initialize2(int** A, int row, int column, int start2) {
+// диагональная змейка
+void createDiagSnake_right(int** A, int row, int column, int start1) {
+    int count_diag = row + column - 1;
+    for (int p = 1; p <= count_diag; ++p) {
+        for (int i = 0, j = p - 1; i < p; ++i, --j) {
+            if (p % 2 == 0) {
+                if (i < row && j < column) {
+                    A[i][j] = start1++;
+                }
+                else {
+                    continue;
+                }
+            }
+            else {
+                if (j < row && i < column) {
+                    A[j][i] = start1++;
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+void createDiagSnake_down(int** A, int row, int column, int start1) {
+    int count_diag = row + column - 1;
+    for (int p = 1; p <= count_diag; ++p) {
+        for (int i = 0, j = p - 1; i < p; ++i, --j) {
+            if (p % 2 != 0) {
+                if (i < row && j < column) {
+                    A[i][j] = start1++;
+                }
+                else continue;
+
+            }
+            else {
+                if (j < row && i < column) {
+                    A[j][i] = start1++;
+                }
+                else continue;
+            }
+        }
+    }
+}
+
+void createUniSpiral(int** A, int row, int column, int start1) {
+    int max = 0;
+    if (row >= column) {
+        max = row;
+    }
+    else {
+        max = column;
+    }
+
+    for (int i = 0; i < max / 2; ++i) {
+        if (start1 > row * column) break;
+        for (int j = i; j < column - i; ++j) {
+            A[i][j] = start1++;
+        }
+        if (start1 > row * column) break;
+        for (int k = i + 1; k < row - i; ++k) {
+            A[k][column - 1 - i] = start1++;
+        }
+        if (start1 > row * column) break;
+        for (int j = column - i - 2; j > i; --j) {
+            A[row - i - 1][j] = start1++;
+        }
+        if (start1 > row * column) break;
+        for (int k = row - i - 1; k > i; --k) {
+            A[k][i] = start1++;
+        }
+    }
+    if ((row % 2) && (row == column))
+        A[row / 2][column / 2] = start1;
+
+}
+
+void printArray(int** A, int row, int column) {
     for (int i = 0; i < row; ++i) {
         for (int j = 0; j < column; ++j) {
-            int k;
-            A[i][j] = start2;
-            ++start2;
+
             std::cout << A[i][j] << "\t";
         }
         std::cout << std::endl;
     }
+
 }
+
+
 
 int main()
-{
+{/*
     //одномерный динамический массив
     int razmer, start;
     std::cout << "razmer :\n";
@@ -47,12 +165,10 @@ int main()
     for (int i = 0; i < razmer; i++) {
         a[i] = start + i;
     }
-
     for (int i = 0; i < razmer; i++)
         std::cout << a[i] << " ";
-
     delete[] a;
-    std::cout << std::endl;
+    std::cout << std::endl;*/
 
     //двумерный динамический массив
     int row, column, start1;
@@ -60,13 +176,55 @@ int main()
     std::cin >> row >> column;
     std::cout << "start number : \n";
     std::cin >> start1;
-    
+
 
     int** A;
-    A = create(row, column); 
-    initialize(A, row, column, start1); 
+    A = create(row, column);
+    std::cout << "\nchoose the task:\n";
+    std::cout << "1. arrage in order\n" << "2.arrage-spiral (only square)\n";
+    std::cout << "3.arrage - snake\n" << "4.arrage - Diag_snake_right\n" << "5.arrage - Diag_snake_down\n"<< "6.arrage - uni-spiral\n";
+    int choice;
+    std::cout << "\nyour choice :" << std::endl;
+    std::cin >> choice;
     std::cout << std::endl;
-    initialize2(A, row, column,start1);
+    switch (choice) {
+    case 1: std::cout << "arrage in order" << std::endl;
+        initialize(A, row, column, start1);
+        std::cout << std::endl;
+        break;
+
+    case 2:std::cout << "arrage-spiral" << std::endl;
+        createSpiral(A, row, column, start1);
+        printArray(A, row, column);
+        std::cout << std::endl;
+        break;
+
+    case 3:std::cout << "arrage-snake" << std::endl;
+        createSnake(A, row, column, start1);
+        printArray(A, row, column);
+        std::cout << std::endl;
+        break;
+
+    case 4:std::cout << "arrage-Diag_snake_right" << std::endl;
+        createDiagSnake_right(A, row, column, start1);
+        printArray(A, row, column);
+        std::cout << std::endl;
+        break;
+
+    case 5:std::cout << "arrage-Diag_snake_down" << std::endl;
+        createDiagSnake_down(A, row, column, start1);
+        printArray(A, row, column);
+        std::cout << std::endl;
+        break;
+
+    case 6 :std::cout << "arrage-UNIspiral" << std::endl;
+        createUniSpiral(A, row, column, start1);
+        printArray(A, row, column);
+        std::cout << std::endl;
+        break;
+
+    default:std::cout << "\nsorry, you put a wrong number...\n";
+    }
 
     for (int i = 0; i < row; i++)
     {
@@ -74,4 +232,5 @@ int main()
     }
     delete[] A;
 }
+
 
--- 08_11_2021/source/task 1 din array.cpp
+++ 08_11_2021/source/task 1 din array.cpp
@@ -5,7 +5,7 @@
 
 int** create(int row, int column) {
     int** A;
-    A = new int *[row];
+    A = new int* [row];
     for (int i = 0; i < row; ++i) {
         A[i] = new int[column];
     }
@@ -15,7 +15,7 @@ int** create(int row, int column) {
 void initialize(int** A, int row, int column, int start1) {
     for (int i = 0; i < row; ++i) {
         for (int j = 0; j < column; ++j) {
-            
+
             A[i][j] = start1;
             ++start1;
             std::cout << A[i][j] << "\t";
@@ -24,23 +24,23 @@ void initialize(int** A, int row, int column, int start1) {
     }
 }
 // квадратная спираль
-void createSpiral (int** A, int row, int column, int start1) {
-    
-        for (int i = 0; i < column/2 +1; ++i) {
-        for (int j = i; j < column-i; ++j) {  
+void createSpiral(int** A, int row, int column, int start1) {
+
+    for (int i = 0; i < column / 2 + 1; ++i) {
+        for (int j = i; j < column - i; ++j) {
             A[i][j] = start1++;
         }
-        for (int k = i+1; k < column - i; ++k) {
-            A[k][column -1 -i] = start1++;
+        for (int k = i + 1; k < column - i; ++k) {
+            A[k][column - 1 - i] = start1++;
         }
-        for (int j = column -i-2; j >=i; --j) {
-            A[column-i-1][j] = start1++;
+        for (int j = column - i - 2; j >= i; --j) {
+            A[column - i - 1][j] = start1++;
         }
-        for (int k = column -2 -i; k > i; --k) {
+        for (int k = column - 2 - i; k > i; --k) {
             A[k][i] = start1++;
-        }    
+        }
     }
-        
+
 }
 
 // змейка
@@ -48,12 +48,12 @@ void createSnake(int** A, int row, int column, int start1) {
 
     for (int i = 0; i < row; ++i) {
         if (i % 2 == 0) {
-            for (int j =0; j < column; ++j) {
+            for (int j = 0; j < column; ++j) {
                 A[i][j] = start1++;
             }
         }
         if (i % 2 != 0) {
-            for (int j = column-1; j >= 0; --j) {
+            for (int j = column - 1; j >= 0; --j) {
 
                 A[i][j] = start1++;
             }
@@ -64,18 +64,21 @@ void createSnake(int** A, int row, int column, int start1) {
 // диагональная змейка
 void createDiagSnake_right(int** A, int row, int column, int start1) {
     int count_diag = row + column - 1;
-    for (int p = 1; p <= count_diag; ++p) {  
+    for (int p = 1; p <= count_diag; ++p) {
         for (int i = 0, j = p - 1; i < p; ++i, --j) {
             if (p % 2 == 0) {
                 if (i < row && j < column) {
                     A[i][j] = start1++;
-                } else {
+                }
+                else {
                     continue;
                 }
-            } else {
+            }
+            else {
                 if (j < row && i < column) {
                     A[j][i] = start1++;
-                } else {
+                }
+                else {
                     continue;
                 }
             }
@@ -90,26 +93,64 @@ void createDiagSnake_down(int** A, int row, int column, int start1) {
             if (p % 2 != 0) {
                 if (i < row && j < column) {
                     A[i][j] = start1++;
-                } else continue;
-                
-            } else {
+                }
+                else continue;
+
+            }
+            else {
                 if (j < row && i < column) {
                     A[j][i] = start1++;
-                } else continue;
+                }
+                else continue;
             }
         }
     }
 }
 
-void printArray (int** A, int row, int column) {
+void createUniSpiral(int** A, int row, int column, int start1) {
+    int max = 0;
+    if (row >= column) {
+        max = row;
+    }
+    else {
+        max = column;
+    }
+
+    for (int i = 0; i < max / 2; ++i) {
+        if (start1 > row * column) break;
+        for (int j = i; j < column - i; ++j) {
+            A[i][j] = start1++;
+        }
+        if (start1 > row * column) break;
+        for (int k = i + 1; k < row - i; ++k) {
+            A[k][column - 1 - i] = start1++;
+        }
+        if (start1 > row * column) break;
+        for (int j = column - i - 2; j > i; --j) {
+            A[row - i - 1][j] = start1++;
+        }
+        if (start1 > row * column) break;
+        for (int k = row - i - 1; k > i; --k) {
+            A[k][i] = start1++;
+        }
+    }
+    if ((row % 2) && (row == column))
+        A[row / 2][column / 2] = start1;
+
+}
+
+void printArray(int** A, int row, int column) {
     for (int i = 0; i < row; ++i) {
         for (int j = 0; j < column; ++j) {
-            
+
             std::cout << A[i][j] << "\t";
         }
         std::cout << std::endl;
     }
+
 }
+
+
 
 int main()
 {/*
@@ -124,10 +165,8 @@ int main()
     for (int i = 0; i < razmer; i++) {
         a[i] = start + i;
     }
-
     for (int i = 0; i < razmer; i++)
         std::cout << a[i] << " ";
-
     delete[] a;
     std::cout << std::endl;*/
 
@@ -143,42 +182,49 @@ int main()
     A = create(row, column);
     std::cout << "\nchoose the task:\n";
     std::cout << "1. arrage in order\n" << "2.arrage-spiral (only square)\n";
-    std::cout << "3.arrage - snake\n" << "4.arrage - Diag_snake_right\n" << "5.arrage - Diag_snake_down\n";
+    std::cout << "3.arrage - snake\n" << "4.arrage - Diag_snake_right\n" << "5.arrage - Diag_snake_down\n"<< "6.arrage - uni-spiral\n";
     int choice;
-    std::cout <<"\nyour choice :" << std::endl;
+    std::cout << "\nyour choice :" << std::endl;
     std::cin >> choice;
     std::cout << std::endl;
     switch (choice) {
     case 1: std::cout << "arrage in order" << std::endl;
-    initialize(A, row, column, start1);
-    std::cout << std::endl;
-    break;
+        initialize(A, row, column, start1);
+        std::cout << std::endl;
+        break;
 
     case 2:std::cout << "arrage-spiral" << std::endl;
-    createSpiral(A, row, column, start1);
-    printArray(A, row, column);
-    std::cout << std::endl;
-    break;
+        createSpiral(A, row, column, start1);
+        printArray(A, row, column);
+        std::cout << std::endl;
+        break;
 
     case 3:std::cout << "arrage-snake" << std::endl;
-    createSnake(A, row, column, start1);
-    printArray(A, row, column);
-    std::cout << std::endl;
-    break;
+        createSnake(A, row, column, start1);
+        printArray(A, row, column);
+        std::cout << std::endl;
+        break;
 
     case 4:std::cout << "arrage-Diag_snake_right" << std::endl;
-    createDiagSnake_right(A, row, column, start1);
-    printArray(A, row, column);
-    std::cout << std::endl;
-    break;
+        createDiagSnake_right(A, row, column, start1);
+        printArray(A, row, column);
+        std::cout << std::endl;
+        break;
 
     case 5:std::cout << "arrage-Diag_snake_down" << std::endl;
-    createDiagSnake_down(A, row, column, start1);
-    printArray(A, row, column);
-    std::cout << std::endl;
+        createDiagSnake_down(A, row, column, start1);
+        printArray(A, row, column);
+        std::cout << std::endl;
+        break;
+
+    case 6 :std::cout << "arrage-UNIspiral" << std::endl;
+        createUniSpiral(A, row, column, start1);
+        printArray(A, row, column);
+        std::cout << std::endl;
+        break;
 
     default:std::cout << "\nsorry, you put a wrong number...\n";
-}
+    }
 
     for (int i = 0; i < row; i++)
     {
@@ -186,4 +232,5 @@ int main()
     }
     delete[] A;
 }
+
 
